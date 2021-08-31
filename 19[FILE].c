547a5aacc9bed37c9a710925455b8a655555f489
KK6117 <krishnakamalmm.kkm@gmail.com>
2021-08-31 06:51:03 +0530

    the new changes are reflected here '

--- 19[FILE].c
+++ 19[FILE].c
@@ -8,10 +8,10 @@ printf("DATA INPUT\n\n");
 f=fopen("34k.txt", "w");
 fscanf(stdin, "%[^\n]", c);
 fprintf(f, "%s", c);
-while((c=getchar())!=EOF)
-{
-putc(c, f);
-}
+// while((c=getchar())!=EOF)
+// {
+// putc(c, f);
+// }
 fclose(f);
 printf("DATA OUTPUT\n\n");
 f=fopen("34k.txt", "r");
